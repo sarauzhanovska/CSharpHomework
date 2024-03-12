0b451537875e511a23d4a9406fc6267f5a4a99c5
sarauzhanovska <uzhanovskasara@gmail.com>
2024-03-12 14:09:48 +0100

    Homework02

---
+++ Homework02/Homework02/Exercises/Program.cs
@@ -0,0 +1,136 @@
+﻿
+#region Exercises from presentation
+
+//Exercise 5
+
+int[] ArrayOfNumbers = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Enter a number");
+    ArrayOfNumbers[i] = int.Parse(Console.ReadLine());
+
+}
+int sumOfNum = 0;
+foreach (int number in ArrayOfNumbers)
+{
+    sumOfNum += number;
+}
+
+Console.WriteLine("The sum of the entered numbers is: " + sumOfNum);
+
+Console.ReadLine();
+
+//Exercise 6
+
+
+string[] names = new string[10];
+int currentIndex = 0;
+
+while (true)
+{
+    Console.WriteLine("Enter name");
+    string newName = Console.ReadLine();
+
+    names[currentIndex] = newName;
+    currentIndex++;
+
+    Console.WriteLine("Do want to enter another name(Y / N)?");
+    char choice;
+    while (true)
+    {
+
+        choice = char.ToUpper(Console.ReadKey().KeyChar);
+
+        if (choice == 'Y' || choice == 'N')
+        {
+            Console.WriteLine();
+            break;
+        }
+        else 
+        {
+            Console.WriteLine("You've entered an invalid letter. Please enter Y or N.");
+            
+        }
+    }
+    if (choice == 'N')
+    {
+        break;
+    }
+
+}
+for (int i = 0; i < currentIndex; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+Console.ReadLine() ;
+
+#endregion
+
+#region Homework Exercises
+// TASK 1
+
+int[] numbers = new int[6];
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine("Enter a number: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+int SumOfEven = 0;
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        SumOfEven += number;
+    }
+}
+
+Console.WriteLine("Result: " + SumOfEven);
+
+// TASK 2
+
+
+string[] studentsG1 = new string[5] { "Sara", "Radica", "Anamarija", "Keti", "Simona" };
+string[] studentsG2 = new string[5] { "Robert", "Luka", "Mihail", "Filip", "Jovan" };
+
+int groupNum;
+while (true)
+{
+    Console.WriteLine("Enter students group. 1 or 2");
+    bool isParsed = int.TryParse(Console.ReadLine(), out groupNum);
+
+
+    if (!isParsed)
+    {
+        Console.WriteLine("You've entered invalid number");
+    }
+
+    else if (groupNum != 1 && groupNum != 2)
+    {
+        Console.WriteLine("Please enter either 1 or 2");
+
+    }
+    else
+    {
+        Console.WriteLine("Students in Group " + groupNum + ":");
+    }
+
+
+    if (groupNum == 1)
+    {
+        foreach (string student in studentsG1)
+        {
+            Console.WriteLine(student);
+        }
+    }
+    else if (groupNum == 2)
+    {
+        foreach (string student in studentsG2)
+        {
+            Console.WriteLine(student);
+        }
+        break;
+    }
+}
+#endregion
---
+++ Homework03/Homework03/Exercises/Program.cs
@@ -0,0 +1,88 @@
+﻿#region Homework Exercises
+
+// Exercise 1
+
+
+DateTime birthDate;
+
+while (true)
+{
+    Console.WriteLine("Enter your birthday date (MM/dd/yyyy): ");
+    string input = Console.ReadLine();
+
+    if (!DateTime.TryParse(input, out birthDate))
+    {
+        Console.WriteLine("Invalid date format. Please enter the date in MM/dd/yyyy format.");
+        continue;
+    }
+
+    if (birthDate > DateTime.Today)
+    {
+        Console.WriteLine("Invalid date. Please enter a date in the past.");
+        continue;
+    }
+
+    break;
+}
+
+int age = CalculateAge(birthDate);
+Console.WriteLine($"You are {age} years old.");
+
+
+static int CalculateAge(DateTime birthDate)
+{
+    DateTime currentDate = DateTime.Today;
+    int age = currentDate.Year - birthDate.Year;
+
+    if (birthDate.Date > currentDate.AddYears(-age))
+    {
+        age--;
+    }
+
+    return age;
+}
+
+
+Console.ReadLine();
+
+//Exercise 2
+
+
+Console.WriteLine("Enter the date and time (MM/dd/yyyy hh:mm:ss): ");
+DateTime dateTime = DateTime.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the wanted format (1 - MM/dd/yyyy, 2 - MM/dd/yyyy hh:mm:ss, 3 - dddd, dd MMMM yyyy HH:mm:ss, 4 - MM.dd.yyyy): ");
+bool isParsed = int.TryParse(Console.ReadLine(), out int formatChoise);
+
+string formattedDateTime = FormatDateTime(dateTime, formatChoise);
+Console.WriteLine("Formatted Date and Time: " + formattedDateTime);
+
+
+static string FormatDateTime(DateTime dateTime, int formatChoice)
+{
+    string formattedDateTime = "";
+    switch (formatChoice)
+    {
+        case 1:
+            formattedDateTime = dateTime.ToString("MM/dd/yyyy");
+            break;
+        case 2:
+            formattedDateTime = dateTime.ToString("MM/dd/yyyy hh:mm:ss");
+            break;
+        case 3:
+            formattedDateTime = dateTime.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+            break;
+        case 4:
+            formattedDateTime = dateTime.ToString("MM.dd.yyyy");
+            break;
+        default:
+            formattedDateTime = "Format choice is not allowed";
+            break;
+    }
+    return formattedDateTime;
+}
+
+
+
+
+#endregion
