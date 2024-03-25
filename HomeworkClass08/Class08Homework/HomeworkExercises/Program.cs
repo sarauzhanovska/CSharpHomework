19496a7ae5eb6d9f8d7e5969fcf11df6d28ede0d
sarauzhanovska <uzhanovskasara@gmail.com>
2024-03-25 20:26:02 +0100

    Class08Homework

---
+++ HomeworkClass08/Class08Homework/HomeworkExercises/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// TASK 1
+
+#region Homework Exercises
+
+
+Queue<int> queue = new Queue<int>();
+
+bool tryAgain = true;
+
+
+while (tryAgain)
+{
+    Console.WriteLine("Enter a number: ");
+    bool isParsed = int.TryParse(Console.ReadLine(), out int input);
+    if (!isParsed)
+    {
+        Console.WriteLine("You've entered invalid number.");
+    }
+    else
+    {
+        queue.Enqueue(input);
+        Console.WriteLine("Do you want to enter another number? Y or N");
+        string secondInput = Console.ReadLine().ToUpper();
+        if (secondInput.ToUpper() != "Y")
+        {
+            tryAgain = false;
+        }
+
+    }
+
+}
+
+Console.WriteLine("Numbers: ");
+foreach (int num in queue)
+{
+    Console.WriteLine(num);
+}
+
+
+
+
+#endregion
